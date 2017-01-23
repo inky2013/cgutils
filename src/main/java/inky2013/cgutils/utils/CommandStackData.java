51acaa7c5c8774114044e74f825f596321cecc08
inky <ethanbrews@icloud.com>
2017-01-23 22:48:56 +0000

    New options for command stack

---
+++ src/main/java/inky2013/cgutils/utils/CommandStackData.java
@@ -0,0 +1,14 @@
+package inky2013.cgutils.utils;
+
+/**
+ * Created by ethan on 03/01/2017.
+ */
+public class CommandStackData {
+
+    public String name = "testcmd";
+    public boolean requiresop = false;
+    public String[] commands = {"say hi"};
+    public boolean requiresExactArgs = false;
+    public int argsAmt = 0;
+    public boolean executeAsServer = false;
+}
