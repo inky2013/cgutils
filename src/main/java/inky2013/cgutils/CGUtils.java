af93e92570e06f2b72b7a074870f86399e5056ce
inky <ethanbrews@icloud.com>
2017-01-23 22:46:58 +0000

    New config for sudo command

--- src/main/java/inky2013/cgutils/CGUtils.java
+++ src/main/java/inky2013/cgutils/CGUtils.java
@@ -1,81 +1,104 @@
 package inky2013.cgutils;
 
-import inky2013.cgutils.commands.PrintTextTranslation;
-import inky2013.cgutils.commands.Update;
-import inky2013.cgutils.proxy.CommonProxy;
-import inky2013.cgutils.utils.CGCreativeTab;
+import inky2013.cgutils.commands.*;
 import inky2013.cgutils.utils.CGLogger;
+import inky2013.cgutils.utils.CommandStackReader;
+import inky2013.cgutils.utils.CommandStackRegistry;
+import inky2013.cgutils.utils.UpdateReader;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.world.GameRules;
 import net.minecraftforge.common.config.Configuration;
+import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.SidedProxy;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
-import net.minecraftforge.fml.common.network.NetworkRegistry;
-import net.minecraftforge.fml.common.network.simpleimpl.SimpleNetworkWrapper;
-import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.lang3.JavaVersion;
+import org.apache.commons.lang3.SystemUtils;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
  * Created by ethan on 02/01/2017.
  */
-@Mod(modid = Constants.modid, name = Constants.modname, version = Constants.version, updateJSON = "https://inky2013.github.io/cgutils/update.json")
+@Mod(modid = Constants.modid, name = Constants.modname, version = Constants.version, updateJSON = "https://inky2013.github.io/cgutils/update.json", acceptedMinecraftVersions = "[1.10.2,1.11.2]")
 public class CGUtils {
 
-    @SidedProxy(clientSide = Constants.clientproxy,serverSide = Constants.serverproxy)
-    public static CommonProxy proxy;
-
     public static Configuration config;
-    public static final SimpleNetworkWrapper packetHandler = NetworkRegistry.INSTANCE.newSimpleChannel(Constants.modid);
 
     public static boolean registerCommandStacker = true;
     public static boolean registerUpdateCommand = true;
-    public static int[] oreSpawnAllow = {
-            10 //Ender Ore
-    };
-    public static String[] nocopy;
+    public static boolean registerSoundCommand = true;
+    public static boolean registerSudoCommand = true;
+    public static boolean registerNameCommand = true;
+    public static String[] sudoBlacklist;
+    public static String[] sudoWhitelist;
     public static String workingDirectory = System.getProperty("user.dir");
+    public static UpdateInformation[] updates;
 
     @Mod.Instance(Constants.modid)
     public static CGUtils instance;
 
-    public static CGCreativeTab creativeTab = new CGCreativeTab();
-
     public Update updatecmd = new Update();
-
+    public CommandStackRegistry stackRegistry = new CommandStackRegistry();
+    public MinecraftServer server;
 
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent e) {
-        proxy.preInit(e);
+        CGLogger.info(String.format("Starting %s", Constants.modid));
         config = new Configuration(e.getSuggestedConfigurationFile());
         getCfg();
 
     }
 
     @Mod.EventHandler
-    public void init(FMLInitializationEvent e) {
-        proxy.init(e);
-    }
-
-    @Mod.EventHandler
     public void postInit(FMLPostInitializationEvent e) {
-        proxy.postInit(e);
+        reloadUpdates();
 
     }
+
+    public void reloadUpdates() {
+        CGUtils.instance.updatecmd.clear();
+        UpdateInformation[] u = getUpdateCommands();
+        for (UpdateInformation i : u) {
+            CGUtils.instance.updatecmd.registerDownload(i);
+        }
+    }
+
+    public UpdateInformation[] getUpdateCommands() {
+        if (CGUtils.registerUpdateCommand) {
+            updates = UpdateReader.getUpdatesFromFile();
+            for (int i = 0; i < updates.length; i++) {
+                CGLogger.info("Found update '" + updates[i].name + "'");
+            }
+        }
+        return updates;
+    }
+
     @Mod.EventHandler
     public void serverLoad(FMLServerStartingEvent event)
     {
+        server = event.getServer();
         event.registerServerCommand(new PrintTextTranslation());
         if (registerCommandStacker) {
-            for (int i = 0; i < CommonProxy.stackedcommands.length; i++) {
-                event.registerServerCommand(CommonProxy.stackedcommands[i]);
-            }
+            event.getServer().worldServerForDimension(0).getGameRules().setOrCreateGameRule("commandStackOutput", "false");
+            event.registerServerCommand(new ReloadStacks());
+            stackRegistry.registerCommands(event);
         }
         if (registerUpdateCommand) {
             event.registerServerCommand(updatecmd);
+        }
+        if (registerSoundCommand) {
+            event.registerServerCommand(new Sound());
+        }
+        if (registerSudoCommand) {
+            event.registerServerCommand(new Sudo());
+        }
+        if (registerNameCommand) {
+            event.registerServerCommand(new GetName());
         }
     }
 
@@ -83,11 +106,13 @@ public class CGUtils {
     public static void getCfg() {
         try {
             config.load();
-
-            //oreSpawnAllow[0] = config.get("WorldGen", "enderOre",10,"Rarity of ender ore. Set to 0 to disable the ore. Default is 10").getInt();
-            nocopy = config.get("Item Duplication", "blacklist",new String[]{}, "Stuff you shouldn't be able to copy.").getStringList();
             registerCommandStacker = config.get("Commands", "registerCommandStacker",true,"Register the command stacker (runs preset commands set in the commandstacks directory)").getBoolean();
             registerUpdateCommand = config.get("Commands", "registerUpdateCommand",true,"Register the command stacker (downloads a file set in the updates directory)").getBoolean();
+            registerSoundCommand = config.get("Commands", "registerSoundCommand",true,"Adds a /sound command as a simpler alternative to /playsound").getBoolean();
+            registerNameCommand = config.get("Commands", "registerNameCommand",true,"Adds a /getname command to get the name of the sender.").getBoolean();
+            registerSudoCommand = config.get("Command", "registerSudoCommand",true,"Register sudo command to run any command as op. This allows fake players to run admin command. Actual players can't use the command unless they are admin or whitelisted.").getBoolean();
+            sudoWhitelist = config.get("Command", "sudoWhitelist", new String[]{}, "Whitelisted entity names for /sudo command. If left empty the blacklist will be used instead.").getStringList();
+            sudoBlacklist = config.get("Command", "sudoBlacklist", new String[]{}, "Blacklisted entity names for /sudo command. If this and sudoWhitelist are left blank everything except non-opped players will be allowed to use the command.").getStringList();
 
         } catch (Exception e) {
             CGLogger.warn("Failed to read config.");
